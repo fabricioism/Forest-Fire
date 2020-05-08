687e5c8326a26477b91075888fcfc3cfd15c64ce
fabricio <fabriciomurillo24@gmail.com>
2020-05-08 15:02:37 -0600

    Scatter plots

--- Forest-fire.R
+++ Forest-fire.R
@@ -7,32 +7,18 @@ library(purrr)
 #Load Data
 forest_fire_data <- read.csv('forestfires.csv')
 
-#BOX PLOTS
-#Function by Month
-box_plot_month<- function(x, y){
-  ggplot(data = forest_fire_data) +
-    aes_string(x = x, y = y) +
-    geom_boxplot() +
-    labs(title = "Distribution by month") +
-    theme(panel.background = element_rect(fill = 'white'))
-}
 
-#Function by Day
-box_plot_day<- function(x, y){
-  ggplot(data = forest_fire_data) +
+affect_forest_scatter <- function(x,y){
+  ggplot2::ggplot(data = forest_fire_data) +
     aes_string(x = x, y = y) +
-    geom_boxplot() +
-    labs(title = "Distribution by Day") +
-    theme(panel.background = element_rect(fill = 'white'))
+    geom_point(alpha = 0.3) +
+    theme(panel.background = element_rect(fill = "white"))
 }
 
 #Variables
-x_var_day<- names(forest_fire_data)[4]
-x_var_month<- names(forest_fire_data)[3]
-y_var<- names(forest_fire_data)[5:12] 
+x_var <- names(forest_fire_data)[5:12] 
+y_var<- names(forest_fire_data)[13] 
 
 
 #Mapping
-fires_by_month <- map2(x_var_month, y_var, box_plot_month)
-fires_by_day <- map2(x_var_day, y_var, box_plot_day)
-
+affect_forest <- purrr::map2(x_var, y_var, affect_forest_scatter)
