f85b9c7fa8a71a0dd25bc1f2127fe129056bb879
fabricio <fabriciomurillo24@gmail.com>
2020-05-08 02:03:34 -0600

    Reordered bar charts.

--- Forest-fire.R
+++ Forest-fire.R
@@ -1,2 +1,41 @@
-library("readr")
+#Libraries
+library(readr)
+library(dplyr)
+library(ggplot2)
+#Load Data
 forest_fire_data <- read.csv('forestfires.csv')
+
+#Group by day
+forest_fires_day <- forest_fire_data %>%
+  group_by(day) %>%
+  summarise(total_fires = n())
+
+#reordered
+forest_fires_day<- forest_fires_day %>% 
+  mutate(day = factor(day, levels = c("sun", "mon", "tue", "wed", 
+  "thu", "fri", "sat")))
+
+#Graph
+ggplot(data = forest_fires_day) +
+  aes(x = day, y = total_fires) +
+  geom_bar(stat = 'identity') +
+  labs(title = "Fires by Day", x = "Day", y = "Total fires") +
+  theme(panel.background = element_rect(fill = 'white'))
+
+
+#Group by month
+forest_fires_month <- forest_fire_data %>%
+  group_by(month) %>%
+  summarise(total_fires = n())
+
+#reordered
+forest_fires_month<- forest_fires_month %>% 
+  mutate(month = factor(month, levels = c("jan", "feb", "mar", 
+  "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec")))
+
+#Graph
+ggplot(data = forest_fires_month) +
+  aes(x = month, y = total_fires) +
+  geom_bar(stat = 'identity') +
+  labs(title = "Fires by Month", x = "Month", y = "Total fires") +
+  theme(panel.background = element_rect(fill = 'white'))
